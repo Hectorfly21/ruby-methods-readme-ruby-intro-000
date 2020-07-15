b99f4ff58a5bc440e4abc6ad21ed67d65940cb70
Hector <hectorfly21@gmail.com>
2020-07-15 17:59:07 +0000

    Automatically backed up by Learn

--- ExtaExample.rb
+++ ExtaExample.rb
@@ -1,2 +1,13 @@
 def say_hello_world_ten_times
   phrase = "Welcome"
+  puts phrase
+  puts prhase 
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
